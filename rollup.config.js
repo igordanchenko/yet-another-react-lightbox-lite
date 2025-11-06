531bf650913a6d3d8aafa86e03065b6480efa420
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2025-11-05 17:30:05 -0800

    chore: streamline rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,17 @@
 import dts from "rollup-plugin-dts";
 import typescript from "@rollup/plugin-typescript";
 
-const config = {
+export default [
+  [
+    typescript({
+      include: ["src/**/*"],
+      compilerOptions: { removeComments: true },
+    }),
+  ],
+  [dts()],
+].map((plugins) => ({
   input: "src/index.ts",
   output: [{ dir: "dist", format: "esm" }],
   external: ["react", "react/jsx-runtime", "react-dom"],
-};
-
-export default [
-  {
-    ...config,
-    plugins: [
-      typescript({
-        include: ["src/**/*"],
-        compilerOptions: { removeComments: true },
-      }),
-    ],
-  },
-  {
-    ...config,
-    plugins: [dts()],
-  },
-];
+  plugins,
+}));
